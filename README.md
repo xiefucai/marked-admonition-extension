84a2764869b1e0869dca6a768efec536ec0f0dde
fucai.xie <fucai.xie@razer.com>
2022-09-18 21:21:40 +0800

    release v0.0.1

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# marked-admonition
-an admonition extension for marked
+# marked-admonition-extension
+an admonition extension for [marked](https://github.com/markedjs/marked), both support `ESM` and `CJS`.
+
+## Usage
+you can use `abstract`、`attention`、`bug`、`caution`、`danger`、`error`、`example`、`failure`、`hint`、`info`、`note`、`question`、`quote`、`success`、`tip`、`warning` as their theme;
+
+the below code can be used in node.js, base CommonJs.
+``` js
+const { marked } = require('marked');
+const admonition = require('marked-admonition-extension');
+
+// import the css file
+require('marked/index.css');
+
+marked.use(admonition.default);
+
+const content = marked.parse(`!!! info Publish ESM and CJS in a single package
+In the past decade, due to the lack of a standard module system of \`JavaScript\`, **CommonJS** (a.k.a the \`require('xxx')\` and \`module.exports\` syntax) has been the way how Node.js and NPM packages work. Until 2015, when ECMAScript modules finally show up as the standard solution, the community start migrating to native ESM gradually.
+!!!`);
+
+console.log(content);
+```
+it will output
+``` html
+<div class="admonition admonition-info">
+    <p class="admonition-title">Publish ESM and CJS in a single package</p>
+    <p>In the past decade, due to the lack of a standard module system of <code>JavaScript</code>, <strong>CommonJS</strong> (a.k.a the <code>require(&#39;xxx&#39;)</code> and <code>module.exports</code> syntax) has been the way how Node.js and NPM packages work. Until 2015, when ECMAScript modules finally show up as the standard solution, the community start migrating to native ESM gradually.</p>
+
+    </div>
+```
+
+## Demo
+
+Checkout the [demo page](https://typescript-cbre5j.stackblitz.io) to see marked in action ⛹️.
+Or you can edit and view preview result in [markdown editor](https://xiefucai.gitee.io/article/**).
+
+## License
+
+Copyright (c) 2011-2022, Christopher Jeffrey. (MIT License)
---
+++ src/index.ts
@@ -0,0 +1,92 @@
+﻿import type { marked } from 'marked';
+
+const types = [
+  "abstract",
+  "attention",
+  "bug",
+  "caution",
+  "danger",
+  "error",
+  "example",
+  "failure",
+  "hint",
+  "info",
+  "note",
+  "question",
+  "quote",
+  "success",
+  "tip",
+  "warning"
+];
+const startReg =
+  new RegExp(`^!!!\\s+(${types.join('|')})\\s+(.*)$`);
+  // /^!!!\s+(note|abstract|info|tip|success|question|warning|failure|danger|bug|example|quote|hint|caution|error|attention)\s+(.*)$/
+const endReg = /^!!!\s*$/;
+const debug = false;
+
+const admonitionPlugin: marked.TokenizerExtension | marked.RendererExtension = {
+  name: 'admonition',
+  level: 'block',
+  start(this: marked.TokenizerThis, src: string) {
+    const index = src.match(new RegExp(`(^|[\\r\\n])!!!\\s+(${types.join('|')})\\s+(.*)`))?.index;
+    debug && console.log('🎋[marked start]', src, index);
+    return index;
+  },
+  tokenizer(src: string, _tokens): marked.Tokens.Generic | void {
+    debug && console.log('🗼[marked tokenizer]', src, _tokens);
+    const lines = src.split(/\n/);
+    if (startReg.test(lines[0])) {
+      const section = { x: -1, y: -1 };
+      const sections = [];
+      for (let i = 0, k = lines.length; i < k; i++) {
+        if (startReg.test(lines[i])) {
+          section.x = i;
+        } else if (endReg.test(lines[i])) {
+          section.y = i;
+          if (section.x >= 0) {
+            sections.push({ ...section });
+            section.x = -1;
+            section.y = -1;
+          }
+        }
+      }
+  
+      if (sections.length) {
+        const section = sections[0];
+        const [_, icon, title] = startReg.exec(lines[section.x]) || [];
+        const text = lines.slice(section.x + 1, section.y).join('\n');
+        const raw = lines.slice(section.x, section.y + 1).join('\n');
+        const token = {
+          type: 'admonition',
+          raw,
+          icon,
+          title,
+          text,
+          titleTokens: [],
+          tokens: [],
+          childTokens: ['title', 'text'],
+        };
+  
+        this.lexer.inlineTokens(token.title, token.titleTokens);
+        this.lexer.blockTokens(token.text, token.tokens);
+        return token;
+      }
+    }
+  },
+  renderer(this: marked.RendererThis, token) {
+    debug && console.log('🐉[marked renderer]', this, token);
+    const html = `<div class="admonition admonition-${token.icon}">
+    <p class="admonition-title">${this.parser.parseInline(
+      token.titleTokens, null as any
+    )}</p>
+    ${this.parser.parse(token.tokens!)}
+    </div>`;
+    return html;
+  },
+};
+
+const extensions: (marked.TokenizerExtension | marked.RendererExtension)[] = [admonitionPlugin];
+
+export default <marked.MarkedExtension>{
+  extensions,
+};
